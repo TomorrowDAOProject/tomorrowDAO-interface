ec27bb8572dd1f7371708b63d72de1bf456f90fe
currymz <currymz@tmrwdao.com>
2024-05-13 13:45:24 +0800

    feat: eslint check ci

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import dynamicReq from 'next/dynamic';
 const PageIndex = dynamicReq(() => import('pageComponents/home'), { ssr: false });
 export default function Page() {
+  console.log('page.tsx');
   return <PageIndex />;
 }
 
