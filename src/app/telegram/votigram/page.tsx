6e7670074f5e331238eb2bcd528dc882f6488f5c
evan-spidy <evan@tmrwdao.com>
2024-11-11 17:49:20 +0800

    feat: add richads integration

--- src/app/telegram/votigram/page.tsx
+++ src/app/telegram/votigram/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ConfigProvider from 'components/CmsGlobalConfig';
 import Page from './_page';
 import { host } from 'config';
+import Script from 'next/script';
 
 export default async function page() {
   const cmsRes = await fetch(host + '/cms/items/config', {
@@ -10,9 +11,17 @@ export default async function page() {
   const cmsData = await cmsRes.json();
   if (cmsRes.ok) {
     return (
-      <ConfigProvider config={cmsData.data.config}>
-        <Page />
-      </ConfigProvider>
+      <>
+        <Script
+          src="https://richinfo.co/richpartners/telegram/js/rp-ob.js?pub_id=947286&widget_id=353236"
+          strategy="lazyOnload" // or another strategy like "afterInteractive"
+          data-cfasync="false"
+          async
+        />
+        <ConfigProvider config={cmsData.data.config}>
+          <Page />
+        </ConfigProvider>
+      </>
     );
   }
   return <div>cms api error</div>;
