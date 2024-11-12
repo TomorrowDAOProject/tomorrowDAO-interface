b8a0da47d850d1d116276b7ae4ba393d80ee3d73
evan-spidy <evan@tmrwdao.com>
2024-11-12 15:20:34 +0800

    Revert "Merge branch 'dev' into 699-ui-enhancement-official-list-item"
    This reverts commit cb226b23c9da1c6831b78d469a7ae21805d4d29d, reversing
    changes made to 565d509eba3d2bd5d934af40b8f48bcefc59b3d3.

--- src/app/telegram/votigram/page.tsx
+++ src/app/telegram/votigram/page.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ConfigProvider from 'components/CmsGlobalConfig';
 import Page from './_page';
 import { host } from 'config';
-import Script from 'next/script';
 
 export default async function page() {
   const cmsRes = await fetch(host + '/cms/items/config', {
@@ -11,17 +10,9 @@ export default async function page() {
   const cmsData = await cmsRes.json();
   if (cmsRes.ok) {
     return (
-      <>
-        <Script
-          src="https://richinfo.co/richpartners/in-page/js/richads-ob.js?pubid=947286&siteid=353397"
-          strategy="lazyOnload" // or another strategy like "afterInteractive"
-          data-cfasync="false"
-          async
-        />
-        <ConfigProvider config={cmsData.data.config}>
-          <Page />
-        </ConfigProvider>
-      </>
+      <ConfigProvider config={cmsData.data.config}>
+        <Page />
+      </ConfigProvider>
     );
   }
   return <div>cms api error</div>;
