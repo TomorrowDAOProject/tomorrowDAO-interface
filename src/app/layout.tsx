a23025ee3d68cdddf34c3263f6f0f3943efe76af
evan-spidy <evan@tmrwdao.com>
2024-12-02 14:34:02 +0800

    fix: fix ui for small screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,6 +53,12 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           `}
         </Script>
         <GtagConfigScript />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Unbounded:wght@200..900&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body>
         <noscript>
