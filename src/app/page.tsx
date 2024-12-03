ec72397289cfac0952e127cd3048f6b89ca686e2
Leon-pix-coder <lorenzo@tmrwdao.com>
2024-12-03 14:32:18 +0800

    feat: update home page (#757)
    * feat: update home page styles
    
    * feat: re-commit all files
    
    * feat: update styles of home page
    
    * feat: update padding of app's list
    
    * fix: fixed some style problems
    
    * feat: update style of home page
    
    * fix: fix ui for small screen
    
    * feat: update text of aelf more
    
    * feat: update styles of home page
    
    * feat: format lint
    
    * fix: format code
    
    * fix: update the style lint
    
    * fix: update styles
    
    * feat: update styles
    
    * feat: update sticky height
    
    * feat: update styles of home page
    
    * fea: update codes
    
    * feat: udpate style of menu
    
    * feat: update style lint
    
    * feat: update code formatter
    
    * feat: format and update styles
    
    * feat: update styles
    
    * feat: replace p tag
    
    * feat: remove sm size
    
    * feat: replace p tag
    
    * feat: update text content
    
    * feat: update styles of grid module
    
    * feat: update grid styles
    
    * feat: update list styles
    
    * feat: remove default key
    
    * feat: update media module style
    
    * refactor: refactor context name
    
    * feat: update common button style
    
    * fix: change image size for blogpost
    
    ---------
    
    Co-authored-by: evan-spidy <evan@tmrwdao.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,9 @@
 'use client';
-import React, { useEffect, useRef } from 'react';
-import { Button } from 'aelf-design';
-import { RightOutlined } from '@ant-design/icons';
+import React from 'react';
+import HomePage from './_page';
+
 import './home.css';
-import Link from 'next/link';
-import breadCrumb from 'utils/breadCrumb';
-import { eventBus, ShowHeaderExplore } from 'utils/myEvent';
-import useResponsive from 'hooks/useResponsive';
 
-interface LinkWithRightArrowProps {
-  href: string;
-  children: React.ReactNode;
-}
-const LinkWithRightArrow = (props: LinkWithRightArrowProps) => {
-  return (
-    <Link target="_blank" href={props.href}>
-      <div className="text-colorPrimary flex items-center mt-[16px] text-[16px] font-medium leading-[22px]">
-        <span className="pr-[8px] text-[14px]">{props.children}</span> <RightOutlined />
-      </div>
-    </Link>
-  );
-};
 export default function Page() {
-  // const exploreButtonRef = useRef<HTMLDivElement>(null);
-
-  // useEffect(() => {
-  //   breadCrumb.clearBreadCrumb();
-  // }, []);
-  // const { isLG } = useResponsive();
-  // useEffect(() => {
-  //   const top = isLG ? 64 : 82;
-  //   const observer = new IntersectionObserver(
-  //     (entries) => {
-  //       if (entries[0].isIntersecting) {
-  //         eventBus.emit(ShowHeaderExplore, false);
-  //       } else {
-  //         eventBus.emit(ShowHeaderExplore, true);
-  //       }
-  //     },
-  //     { threshold: 0.5, rootMargin: `0px 0px ${top}px 0px` },
-  //   );
-  //   observer.observe(exploreButtonRef.current as Element);
-  //   return () => {
-  //     observer.disconnect();
-  //   };
-  // }, []);
-  return (
-    <div className="flex">
-      <div className="tmrwdao-grid">
-        <div className="col-3 bg-red-500 offset-6 p-2">Item 1 (3 cols)</div>
-        {/* <div className="col-4 bg-red-500 p-2">Item 2 (4 cols)</div> */}
-        <div className="col-5 bg-red-500 p-2">Item 3 (5 cols)</div>
-      </div>
-    </div>
-  );
+  return <HomePage />;
 }
---
+++ src/components/NavHeader/NavMenuItem/index.tsx
@@ -0,0 +1,25 @@
+import Dropdown from 'components/Dropmenu';
+import { MenuItem } from '..';
+
+const NavMenuItem = ({ item }: { item: MenuItem }) => {
+  return (
+    <div key={item?.key}>
+      {item?.children?.length ? (
+        <Dropdown menu={item.children}>
+          <a
+            onClick={(e) => e.preventDefault()}
+            className="text-[15px] font-medium text-white no-underline font-Montserrat hover:text-mainColor"
+          >
+            <span className="text-[15px] font-medium text-white no-underline font-Montserrat m-[8px]">
+              {item.label}
+            </span>
+          </a>
+        </Dropdown>
+      ) : (
+        item?.label
+      )}
+    </div>
+  );
+};
+
+export default NavMenuItem;
