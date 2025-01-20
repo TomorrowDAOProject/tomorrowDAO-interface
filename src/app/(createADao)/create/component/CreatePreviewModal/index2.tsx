898cb0c73d8ce291cedbe6f3bee688c26915ab34
ronny.tmrwdao <ronny@tmrwdao.com>
2025-01-20 10:30:41 +0800

    Revert "feat: add checkbox"

--- src/app/(createADao)/create/component/CreatePreviewModal/index2.tsx
+++ src/app/(createADao)/create/component/CreatePreviewModal/index2.tsx
@@ -125,7 +125,13 @@ export default function CreatePreviewModal({ open, onClose, onConfirm }: ICreate
 
   const logoUrl = metaData?.metadata?.logoUrl?.[0]?.response?.url;
   return (
-    <Modal isVisible={open}>
+    <Modal
+      // title="Confirm"
+      // footerConfig={{
+      //   buttonList: [{ children: 'Confirm', onClick: onConfirm, disabled: disabled }],
+      // }}
+      isVisible={open}
+    >
       <Flex vertical gap={24}>
         <Flex vertical gap={12}>
           <Flex gap={8} align="center">
