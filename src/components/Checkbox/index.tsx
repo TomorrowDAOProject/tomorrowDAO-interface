38b016c6d7f4f562efeb0cd69862124983bffcc5
donny-huo <dustin@eforest.finance>
2025-01-20 10:30:41 +0800

    Revert "feat: add checkbox"

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,43 +1,45 @@
-import React, { useState } from 'react';
-import { ReactComponent as Checked } from 'assets/revamp-icon/checked.svg';
+import React, { forwardRef, LegacyRef } from 'react';
+import clsx from 'clsx';
 
-interface CheckboxProps {
-  label?: string;
-  checked?: boolean;
-  className?: string;
-  onChange?: (checked: boolean) => void;
+export type Option = {
+  label: string;
+  value: string | number;
+};
+
+interface IRadioProps {
+  options: Option[];
+  value: string | number;
+  onChange?(value: string | number): void;
 }
 
-const Checkbox: React.FC<CheckboxProps> = ({ label, checked = false, className, onChange }) => {
-  const [isChecked, setIsChecked] = useState(checked);
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newChecked = event.target.checked;
-    setIsChecked(newChecked);
-    if (onChange) {
-      onChange(newChecked);
-    }
-  };
-
+const Radio = ({ options, value, onChange }: IRadioProps, ref: LegacyRef<HTMLInputElement>) => {
   return (
-    <div className={`flex items-center gap-[15px] cursor-pointer ${className}`}>
-      <div
-        className={`w-4 h-4 rounded-sm relative flex items-center justify-center ${
-          isChecked ? 'bg-mainColor' : 'bg-darkGray'
-        }`}
-      >
-        <input
-          type="checkbox"
-          checked={isChecked}
-          onChange={handleChange}
-          className="opacity-0 absolute top-0 left-0 w-full h-full cursor-pointer"
-        />
-
-        {isChecked && <Checked />}
-      </div>
-      {label}
+    <div className="flex gap-[10px] flex-col md:flex-row md:gap-[116px]">
+      {options.map((option) => (
+        <label key={option.value} className="flex items-center cursor-pointer">
+          <input
+            ref={ref}
+            type="radio"
+            className="hidden"
+            value={option.value}
+            checked={value === option.value}
+            onChange={() => onChange?.(option.value)}
+          />
+          <span
+            className={clsx(
+              'relative box-border inline-block w-4 h-4 rounded-full border-[1.5px] border-solid border-mainColor mr-2',
+              {
+                "bg-mainColor after:absolute after:left-1/2 after:top-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:content-[''] after:block after:w-2 after:h-2 after:rounded-full after:bg-white":
+                  value === option.value,
+                'bg-white': value !== option.value,
+              },
+            )}
+          />
+          <span className="text-white">{option.label}</span>
+        </label>
+      ))}
     </div>
   );
 };
 
-export default Checkbox;
+export default forwardRef(Radio);
