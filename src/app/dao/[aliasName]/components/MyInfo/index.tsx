906d5677f42d7294f500cb986cbd5c055499e67e
ronny.tmrwdao <ronny@tmrwdao.com>
2025-01-23 11:14:01 +0800

    feat: dao detail

--- src/app/dao/[aliasName]/components/MyInfo/index.tsx
+++ src/app/dao/[aliasName]/components/MyInfo/index.tsx
@@ -225,7 +225,7 @@ export default function MyInfo(props: TInfoTypes) {
 
   return (
     <div
-      className={`my-info-wrap flex flex-col border border-fillBg8 border-solid rounded-lg bg-darkBg lg:px-8 px-[16px] py-6 ${clssName}`}
+      className={`my-info-wrap flex flex-col border border-fillBg8 border-solid rounded-lg bg-darkBg lg:px-6 px-[16px] py-[25px] ${clssName}`}
       style={{
         height: height || 'auto',
       }}
