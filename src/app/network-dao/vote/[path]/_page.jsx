e69c378c238a47ddb67e54095183e8a1c798591b
Leon-pix-coder <lorenzo@tmrwdao.com>
2025-03-13 20:29:43 +1100

    788 update transaction logic to include nanos parameter in nightelf (#929)
    * fix: update params of vote
    
    * feat: update millseconds
    
    * feat: update nanos

--- src/app/network-dao/vote/[path]/_page.jsx
+++ src/app/network-dao/vote/[path]/_page.jsx
@@ -823,6 +823,12 @@ class VoteContainer extends Component {
     const { voteAmountInput, targetPublicKey, electionContractFromExt } =
       this.state;
     const { lockTime } = this.state;
+    let millseconds = 1;
+    const now = new Date().toISOString();
+    const nanosecondsMatch = now.match(/\.(\d{1,9})Z$/);
+    if (nanosecondsMatch) {
+      millseconds = nanosecondsMatch[1]
+    }
 
     const payload = {
       candidatePubkey: targetPublicKey,
@@ -830,7 +836,7 @@ class VoteContainer extends Component {
       amount: voteAmountInput * ELF_DECIMAL,
       endTimestamp: {
         seconds: lockTime.unix(),
-        nanos: lockTime.valueOf() * 1000000,
+        nanos: millseconds * 1000000,
       },
     };
     const chainIdQuery = getChainIdQuery();
